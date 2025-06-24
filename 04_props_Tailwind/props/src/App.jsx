a4eeefa4666da084d3ef7fa1d4f5c5f65ca90b72
Arshpreet0991 <arsh.singh0991@gmail.com>
2025-06-24 12:49:23 -0400

    learning props

---
+++ 04_props_Tailwind/props/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Card } from "./components/Card.jsx";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  console.log(CustomButtons);
+
+  return (
+    <>
+      <h1 className="bg-green-500 text-black p-4 rounded-xl mb-5">
+        Tailwind Test
+      </h1>
+    </>
+  );
+}
+
+export default App;
